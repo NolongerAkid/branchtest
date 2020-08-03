cff0f5fa2c6e3286fb38a7550c472fda75b2b610
jjz <502040653@qq.com>
2020-08-03 01:26:56 -0700

    struct test

--- test1.go
+++ test1.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"strings"
 )
+type student struct {
+	a int
+	b int
+}
 
 func main(){
 	a:="a b c d e"
 	b := strings.Split(a," ")
 	fmt.Print(b)
 	fmt.Println(b[1])
+	A := &student{
+		a :1,
+		b:2,
+	}
+	fmt.Println(A.a)
+	fmt.Println((*A).a)
 
 
 
